2c002fa6973ac7666cb3014dded7f39d886d556e
Jiazz-J <39787706+Jiazz-J@users.noreply.github.com>
2021-06-27 17:26:16 +0530

    Implemented triplets (#2)
    Implemented triplets

--- src/com/kk/arr_vec/triplets/TripletsTwoPointer.java
+++ src/com/kk/arr_vec/triplets/TripletsTwoPointer.java
@@ -1,25 +1,32 @@
 package com.kk.arr_vec.triplets;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TripletsTwoPointer {
 
   public static void main(String[] args) {
     int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 15};
     int target = 18;
+
+    ArrayList<Integer[]> arr = findTriplets(input, target);
+    for (Integer[] arr_ : arr) {
+      System.out.println(Arrays.toString(arr_));
+    }
   }
 
   static ArrayList<Integer[]> findTriplets(int[] input, int target) {
-
-    // int[][] triplets = new int[][]; // Only column initialization will not work
-
     ArrayList<Integer[]> triplets = new ArrayList<>();
-
+    // HashSet<Integer> usedPairs = new HashSet<>();
     for (int i = 0; i < input.length; i++) {
       int compliment = target - input[i];
       int[] pairs = findPairs(input, i + 1, compliment);
+      // if (pairs.length > 0 && !usedPairs.contains(pairs[0]) && !usedPairs.contains(pairs[1])) {
       if (pairs.length > 0) {
+
         triplets.add(new Integer[] {input[i], pairs[0], pairs[1]});
+        /*   usedPairs.add(pairs[0]);
+        usedPairs.add(pairs[1]);*/
       }
     }
     return triplets;
@@ -35,18 +42,16 @@ public class TripletsTwoPointer {
       if (arr[start] + arr[end] == sum) {
         result[0] = arr[start];
         result[1] = arr[end];
-        break;
+        return result;
       } else if (arr[start] + arr[end] > sum) {
         end -= 1;
       } else {
         start += 1;
       }
     }
-    return result;
+    return new int[] {};
   }
 }
-
-
 
   /*static int[] findPairs(int[] input, int start, int target) {
       HashSet<Integer> hashSet = new HashSet<>();
